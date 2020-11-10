011125d9bc0035a07b8ef2336fede5ac01dcb92f
Almonzo Rhoden <almonzo.rhoden@gmail.com>
2020-11-10 17:52:57 -0500

    Updating Portfolio page

---
+++ assets/script/main.js
@@ -0,0 +1,37 @@
+// ALmonzo's JavaScript
+
+// Links user's button sumbit to page desired
+$('#plannerBtn').on('click', function () {
+    console.log('Weather');
+    window.location.href = 'https://almonzorhoden.github.io/day-planner/';
+});
+
+$('#plannerGitBtn').on('click', function () {
+    console.log('Weather Github');
+    window.location.href = 'https://github.com/AlmonzoRhoden/day-planner';
+});
+
+$('#weatherBtn').on('click', function () {
+    console.log('Weather');
+    window.location.href = 'https://almonzorhoden.github.io/weather-dashboard/';
+});
+
+$('#weatherGitBtn').on('click', function () {
+    console.log('Weather Github');
+    window.location.href = 'https://github.com/AlmonzoRhoden/weather-dashboard';
+});
+
+$('#testingBtn').on('click', function () {
+    console.log('Testing');
+    window.location.href = 'https://almonzorhoden.github.io/Covid-19_TestingApp/';
+});
+
+$('#testingGitBtn').on('click', function () {
+    console.log('Testing Github');
+    window.location.href = 'https://github.com/AlmonzoRhoden/Covid-19_TestingApp';
+});
+
+$('#submitBtn').on('click', function () {
+    console.log('Submit');
+    window.location.href = 'mailto:almonzorhode@gmail.com';
+});
